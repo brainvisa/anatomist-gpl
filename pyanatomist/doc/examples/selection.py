871d86acc9ecb4f6000b4a2b4fefb83948d89836
le-troter <le-troter@bioproj.invalid>
2010-04-28 12:14:34 +0000

    update selection.py (compatible with QT4)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-gpl/trunk@37429 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyanatomist/doc/examples/selection.py
+++ pyanatomist/doc/examples/selection.py
@@ -1,39 +1,38 @@
-#  This software and supporting documentation are distributed by
-#      Institut Federatif de Recherche 49
-#      CEA/NeuroSpin, Batiment 145,
-#      91191 Gif-sur-Yvette cedex
-#      France
-#
-# This software is governed by the CeCILL license version 2 under
-# French law and abiding by the rules of distribution of free software.
-# You can  use, modify and/or redistribute the software under the 
-# terms of the CeCILL license version 2 as circulated by CEA, CNRS
-# and INRIA at the following URL "http://www.cecill.info". 
-#
-# As a counterpart to the access to the source code and  rights to copy,
-# modify and redistribute granted by the license, users are provided only
-# with a limited warranty  and the software's author,  the holder of the
-# economic rights,  and the successive licensors  have only  limited
-# liability.
-#
-# In this respect, the user's attention is drawn to the risks associated
-# with loading,  using,  modifying and/or developing or reproducing the
-# software by the user in light of its specific status of free software,
-# that may mean  that it is complicated to manipulate,  and  that  also
-# therefore means  that it is reserved for developers  and  experienced
-# professionals having in-depth computer knowledge. Users are therefore
-# encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
-# same conditions as regards security.
-#
-# The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL license version 2 and that you accept its terms.
-import anatomist.direct.api as anatomist
+# -*- coding: utf-8 -*-
+import anatomist.direct.api as ana
 from soma import aims
+from soma.aims import colormaphints
+import sys, os
 
-# Open Anatomist
-a = anatomist.Anatomist()
+# determine wheter we are using Qt4 or Qt3, and hack a little bit accordingly
+# the boolean qt4 gloabl variable will tell it for later usage
+qt4 = False
+if sys.modules.has_key( 'PyQt4'):
+  qt4 = True
+  from PyQt4 import QtCore, QtGui
+  qt = QtGui
+  from PyQt4.uic import loadUi
+else:
+  import qt, qtui
+  loadUi = qtui.QWidgetFactory.create
+
+# do we have to run QApplication ?
+if qt.qApp.startingUp():
+  qapp = qt.QApplication( sys.argv )
+  runqt = True
+else:
+  runqt = False
+
+# splash
+pix = qt.QPixmap( os.path.expandvars( '$BRAINVISA_SHARE/anatomist-3.2/icons/anatomist.png' ) )
+spl = qt.QSplashScreen( pix )
+spl.show()
+qt.qApp.processEvents()
+
+# start the Anatomist engine, in batch mode (no main window)
+a = ana.Anatomist()
+
+#a = anatomist.Anatomist()
 
 # create a sphere mesh
 m = aims.SurfaceGenerator.sphere( aims.Point3df( 0 ), 100, 100 )
@@ -54,5 +53,11 @@ print "selection in default group", a.getSelection()
 print "selection de", g, g.getSelection()
 sel=g.getSelection()
 #print mesh, sel, mesh == sel[0], mesh is sel[0]
-print 'mesh isSelected:', g.isSelected( mesh )
+#print 'mesh isSelected:', g.isSelected( mesh )
 
+# run Qt
+if runqt:
+  if qt4:
+    qapp.exec_()
+  else:
+    qapp.exec_loop()
