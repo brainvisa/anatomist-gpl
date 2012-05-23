7ddd4cbf11fcb194f8961214012d320fc0c07d25
denisri <denis.riviere.gm@gmail.com>
2012-05-23 14:35:42 +0000

    handle pyanatomist translations (for python modules in anatomist, such as controls descriptions)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-gpl/trunk@49150 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ pyanatomist/share/po/en/pyanatomist.ts
@@ -0,0 +1,211 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>ControlledWindow</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/foldsplit.py" line="416"/>
+        <source>SplitFoldControl</source>
+        <translation>&lt;html&gt;Split Fold Control:
+manually or semi-manually split cortical folds in a graph.
+&lt;tabe&gt;&lt;tr&gt;
+&lt;td&gt;Left click:&lt;/td&gt;&lt;td&gt;prepare a cut with a single point on a fold node&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Ctrl+Left click:&lt;/td&gt;&lt;td&gt;set a cut control point in a series for a dotted line cut&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;S :&lt;/td&gt;&lt;td&gt;show cut line, or confirm split if the cut line is already visible&lt;/td&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ESC:&lt;/td&gt;&lt;td&gt;cancel spliit, erase control points and proposed split line&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Ctrl+Right click:&lt;/td&gt;&lt;td&gt;automatically subdivize a node in several pieces&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Shift+Right click:&lt;/td&gt;&lt;td&gt;automatically subdivize the entire graph by splitting large nodes&lt;/td&gt;&lt;/tr&gt;
+&lt;/table&gt;&lt;tr/&gt;
+Classical rotation, translation and zoom controls also apply.&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="25"/>
+        <source>Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="31"/>
+        <source>Affichage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="41"/>
+        <source>Données :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="51"/>
+        <source>Type :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="66"/>
+        <source>raw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="71"/>
+        <source>mean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="76"/>
+        <source>bad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="81"/>
+        <source>good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="90"/>
+        <source>sillons seulements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="95"/>
+        <source>sillons + relations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="109"/>
+        <source>Différences absolues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="184"/>
+        <source>Fermer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="191"/>
+        <source>Appliquer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GradientObjectParamSelect</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="112"/>
+        <source>Save palette image...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="113"/>
+        <source>Edit gradient information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="114"/>
+        <source>Keep as static palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="119"/>
+        <source>RGB mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="120"/>
+        <source>HSV mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="255"/>
+        <source>Gradient definition:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="277"/>
+        <source>Palette name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SelectionParameters</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="14"/>
+        <source>Form1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="20"/>
+        <source>Edge selection:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="34"/>
+        <source>Edges display mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="59"/>
+        <source>Opacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="73"/>
+        <source>Nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="139"/>
+        <source>100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="116"/>
+        <source>Edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="154"/>
+        <source>Box highlight:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="166"/>
+        <source>Use box highlighting for selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="179"/>
+        <source>Use one box per selected object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="188"/>
+        <source>Box color: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="196"/>
+        <source>Gray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="201"/>
+        <source>As selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="206"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="218"/>
+        <source>Custom box color: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="231"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ pyanatomist/share/po/fr/pyanatomist.ts
@@ -0,0 +1,212 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>ControlledWindow</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/foldsplit.py" line="416"/>
+        <source>SplitFoldControl</source>
+        <translation>&lt;html&gt;Contrôle de découpe de sillons:
+Découpe manuellement ou semi-manuellement des noeuds de graphe cortical.
+&lt;tabe&gt;&lt;tr&gt;
+&lt;td&gt;Left click:&lt;/td&gt;&lt;td&gt;prepare a cut with a single point on a fold node&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Ctrl+Left click:&lt;/td&gt;&lt;td&gt;set a cut control point in a series for a dotted line cut&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;S :&lt;/td&gt;&lt;td&gt;show cut line, or confirm split if the cut line is already visible&lt;/td&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;ESC:&lt;/td&gt;&lt;td&gt;cancel spliit, erase control points and proposed split line&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Ctrl+Right click:&lt;/td&gt;&lt;td&gt;automatically subdivize a node in several pieces&lt;/td&gt;&lt;/tr&gt;
+&lt;tr&gt;&lt;td&gt;Shift+Right click:&lt;/td&gt;&lt;td&gt;automatically subdivize the entire graph by splitting large nodes&lt;/td&gt;&lt;/tr&gt;
+&lt;/table&gt;&lt;tr/&gt;
+Les contrôles classiques de rotation, translation et zoom fonctionnent aussi.
+&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="25"/>
+        <source>Configuration</source>
+        <translation type="unfinished">Configuration</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="31"/>
+        <source>Affichage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="41"/>
+        <source>Données :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="51"/>
+        <source>Type :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="66"/>
+        <source>raw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="71"/>
+        <source>mean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="76"/>
+        <source>bad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="81"/>
+        <source>good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="90"/>
+        <source>sillons seulements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="95"/>
+        <source>sillons + relations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="109"/>
+        <source>Différences absolues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="184"/>
+        <source>Fermer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/modelGraphs.ui" line="191"/>
+        <source>Appliquer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GradientObjectParamSelect</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="112"/>
+        <source>Save palette image...</source>
+        <translation>Sauver la palette comme image</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="113"/>
+        <source>Edit gradient information</source>
+        <translation>Editer les informations de gradient</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="114"/>
+        <source>Keep as static palette</source>
+        <translation>Garder comme palette statique</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="119"/>
+        <source>RGB mode</source>
+        <translation>mode RVB</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="120"/>
+        <source>HSV mode</source>
+        <translation>mode HSV</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="255"/>
+        <source>Gradient definition:</source>
+        <translation>Définition de gradient:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/gradientpalette.py" line="277"/>
+        <source>Palette name:</source>
+        <translation>Nom de palette:</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionParameters</name>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="14"/>
+        <source>Form1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="20"/>
+        <source>Edge selection:</source>
+        <translation>Sélection des relations:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="34"/>
+        <source>Edges display mode:</source>
+        <translation>Mode de visu des relations:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="59"/>
+        <source>Opacity:</source>
+        <translation>Opacité:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="73"/>
+        <source>Nodes</source>
+        <translation>Noeuds</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="139"/>
+        <source>100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="116"/>
+        <source>Edges</source>
+        <translation>Relations</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="154"/>
+        <source>Box highlight:</source>
+        <translation>Visualisation boîtes:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="166"/>
+        <source>Use box highlighting for selection</source>
+        <translation>Utiliser des boîtes pour la sélection</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="179"/>
+        <source>Use one box per selected object</source>
+        <translation>Une boîte par objet sélectionné</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="188"/>
+        <source>Box color: </source>
+        <translation>Couleur de boîte:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="196"/>
+        <source>Gray</source>
+        <translation>Gris</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="201"/>
+        <source>As selection</source>
+        <translation>Comme sélection</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="206"/>
+        <source>Custom</source>
+        <translation>Spécifique</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="218"/>
+        <source>Custom box color: </source>
+        <translation>Couleur spécifique de boîte:</translation>
+    </message>
+    <message>
+        <location filename="../../../../../../neurosvn/anatomist/anatomist-free/trunk/shared/python_plugins/selection-qt4.ui" line="231"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+</TS>
