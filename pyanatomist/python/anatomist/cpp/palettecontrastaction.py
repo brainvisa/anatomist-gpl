5716be500beecd8573998aa2988ca04cef056685
denisri <denis.riviere.gm@gmail.com>
2013-05-22 16:51:09 +0000

    visual feedback when interactively changing palette scaling using the contrast actions
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-gpl/trunk@55872 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyanatomist/python/anatomist/cpp/palettecontrastaction.py
+++ pyanatomist/python/anatomist/cpp/palettecontrastaction.py
@@ -46,6 +46,9 @@ class PaletteContrastAction( anatomist.Action ):
   def startContrast( self, x, y, globx, globy ):
     self._start = ( x, y )
     self._palettes = {}
+    self._showGraphicsView( self.view() )
+    self._tmpitems = []
+    self._drawPaletteInGraphicsView( self.view() )
 
   def moveContrast( self, x, y, globx, globy ):
     win = self.view().aWindow()
@@ -73,6 +76,7 @@ class PaletteContrastAction( anatomist.Action ):
           max=val )
       elif isinstance( o, anatomist.MObject ):
         objs += [ mo for mo in o if mo not in objs ]
+    self._drawPaletteInGraphicsView( self.view() )
 
   def moveContrastMin( self, x, y, globx, globy ):
     win = self.view().aWindow()
@@ -95,6 +99,7 @@ class PaletteContrastAction( anatomist.Action ):
   def stopContrast( self, x, y, globx, globy ):
     del self._start
     del self._palettes
+    self._removeGraphicsView( self.view() )
 
   def resetPalette( self ):
     win = self.view().aWindow()
@@ -108,6 +113,118 @@ class PaletteContrastAction( anatomist.Action ):
           min=pal.min1(), max=pal.max1() )
       elif isinstance( o, anatomist.MObject ):
         objs += [ mo for mo in o if mo not in objs ]
+
+  def _graphicsViewOnWindow( self, view ):
+    glw = view.qglWidget()
+    gv = glw.findChild( QtGui.QGraphicsView )
+    if gv is not None:
+      return gv
+    l = QtGui.QVBoxLayout()
+    glw.setLayout( l )
+    gv = QtGui.QGraphicsView( glw )
+    l.addWidget( gv, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom )
+    gv.setFixedSize( 150, 50 )
+    gv.setFrameStyle( QtGui.QFrame.NoFrame )
+    return gv
+
+  def _showGraphicsView( self, view ):
+    gv = self._graphicsViewOnWindow( view )
+    gv.show()
+
+  def _removeGraphicsView( self, view ):
+    gv = self._graphicsViewOnWindow( view )
+    scene = gv.scene()
+    if scene:
+      for item in self._tmpitems:
+        scene.removeItem( item )
+    self._tmpitems = []
+    gv.hide()
+
+  def _drawPaletteInGraphicsView( self, view ):
+    gv = self._graphicsViewOnWindow( view )
+    objs = view.aWindow().Objects()
+    obj = None
+    for o in objs:
+      pal = o.palette()
+      if pal:
+        obj = o
+        break
+    if obj is None:
+      return
+    w = gv.width() - 12
+    baseh = ( gv.height() - 10 ) *0.33 + 5
+    baseh2 = ( gv.height() - 10 ) *0.66 + 5
+    img = pal.toQImage( w, baseh2 - baseh - 1 )
+    pix = QtGui.QPixmap.fromImage( img )
+    scene = gv.scene()
+    paintpen = QtGui.QPen( QtGui.QColor( 120, 120, 120 ) )
+    if scene is None:
+      scene = QtGui.QGraphicsScene( gv )
+      gv.setScene( scene )
+      scene.addRect( 5, baseh, gv.width() - 10, baseh2 - baseh, paintpen )
+      scene.addLine( 5, baseh, 5, 5, paintpen )
+      scene.addLine( gv.width() - 5, baseh, gv.width() - 5, 5, paintpen )
+    for item in self._tmpitems:
+      scene.removeItem( item )
+    self._tmpitems = []
+    pixitem = QtGui.QGraphicsPixmapItem( pix )
+    tr = pixitem.transform()
+    tr.translate( 6, baseh + 1 )
+    pixitem.setTransform( tr )
+    xmin = 6 + w * pal.min1()
+    xmax = 6 + w * pal.max1()
+    if xmin >= 0 and xmin < w:
+      line = scene.addLine( xmin, baseh2, xmin, gv.height()-5, paintpen )
+      self._tmpitems.append( line )
+    if xmax >= 0 and xmax < w:
+      line = scene.addLine( xmax, baseh2, xmax, gv.height()-5, paintpen )
+      self._tmpitems.append( line )
+    self._tmpitems.append( pixitem )
+    scene.addItem( pixitem )
+    valmin = 0.
+    valmax = 1.
+    glc = obj.glAPI()
+    if glc:
+      extr = glc.glTexExtrema( 0 )
+      valmin = extr.minquant[0]
+      valmax = extr.maxquant[0]
+      del extr, glc
+    palmin = valmin + (valmax - valmin) * pal.min1()
+    palmax = valmin + (valmax - valmin) * pal.max1()
+    text = self._textGraphicsItem( '%.2f' % palmin, xmin, baseh2 + 3,
+      xmax, gv.width() - 5 )
+    scene.addItem( text )
+    self._tmpitems.append( text )
+    text = self._textGraphicsItem( '%.2f' % palmax, xmax, baseh2 + 3,
+      xmin, gv.width() - 5 )
+    scene.addItem( text )
+    self._tmpitems.append( text )
+    text = self._textGraphicsItem( '%.2f' % valmin, 8, 5, gv.width() - 5,
+      gv.width() - 5 )
+    scene.addItem( text )
+    self._tmpitems.append( text )
+    text = self._textGraphicsItem( '%.2f' % valmax, gv.width() - 10, 5,
+      gv.width() - 5, gv.width() - 5 )
+    scene.addItem( text )
+    self._tmpitems.append( text )
+
+  def _textGraphicsItem( self, text, xpos, ypos, xmax, hardmax=None ):
+    text = QtGui.QGraphicsSimpleTextItem( text )
+    font = text.font()
+    font.setPointSize( 6 )
+    text.setFont( font )
+    tr = text.transform()
+    x = xpos + 3
+    w = text.boundingRect().right()
+    if x < xmax and x + w >= xmax - 3:
+      x = xmax - 3 - w
+    if x < 4:
+      x = 4
+    if hardmax is not None and x + w >= hardmax:
+      x = hardmax - w - 3
+    tr.translate( x, ypos )
+    text.setTransform( tr )
+    return text
 
 
 ad = anatomist.ActionDictionary.instance()
