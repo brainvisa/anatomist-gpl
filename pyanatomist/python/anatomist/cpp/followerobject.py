bca826892be2ab0fb64df5251058bfbd79af201f
denisri <denis.riviere.gm@gmail.com>
2013-07-12 12:51:33 +0000

    use SurfaceGenerator to build boxes
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-gpl/trunk@57097 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyanatomist/python/anatomist/cpp/followerobject.py
+++ pyanatomist/python/anatomist/cpp/followerobject.py
@@ -108,18 +108,9 @@ class ObjectFollowerCube( anatomist.ASurface_2 ):
     else:
       bbox = self.boundingbox()
       if len( bbox ) == 2:
-        vert = [ aims.Point3df( bbox[0] ),
-          aims.Point3df( bbox[1][0], bbox[0][1], bbox[0][2] ),
-          aims.Point3df( bbox[0][0], bbox[1][1], bbox[0][2] ),
-          aims.Point3df( bbox[0][0], bbox[0][1], bbox[1][2] ),
-          aims.Point3df( bbox[1][0], bbox[1][1], bbox[0][2] ),
-          aims.Point3df( bbox[1][0], bbox[0][1], bbox[1][2] ),
-          aims.Point3df( bbox[0][0], bbox[1][1], bbox[1][2] ),
-          aims.Point3df( bbox[1] )
-        ]
-        pol = [ [0,1], [0,2], [1,4], [2,4], [0,3], [1,5], [2,6], [4,7], [3,5],
-          [3,6], [5,7], [6,7] ]
-      mesh.vertex().assign( vert )
-      mesh.polygon().assign( [ aims.AimsVector_U32_2(i) for i in pol ] )
+        cube = aims.SurfaceGenerator.parallelepiped_wireframe( 
+          bbox[0], bbox[1] )
+        mesh.vertex().assign( cube.vertex() )
+        mesh.polygon().assign( cube.polygon() )
     self.setChanged()
     del self._redrawing
