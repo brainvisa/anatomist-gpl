4b15107223087360f4c78eab575c244b4593ae15
Denis Rivière <denis.riviere.gm@gmail.com>
2022-12-19 15:27:36 +0100

    port to newer SIP API

--- pyanatomist/src/sip/objectmenu.h
+++ pyanatomist/src/sip/objectmenu.h
@@ -39,64 +39,64 @@
 
 inline PyObject* pyanatomistConvertFrom_anatomist_ObjectMenu( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_anatomist_ObjectMenu, 0 );
+  return sipConvertFromType( a, sipType_anatomist_ObjectMenu, 0 );
 }
 
 
 inline PyObject* pyanatomistConvertFrom_anatomist_ObjectMenuP( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_anatomist_ObjectMenu, 0 );
+  return sipConvertFromType( a, sipType_anatomist_ObjectMenu, 0 );
 }
 
 
 inline PyObject* pyanatomistConvertFrom_anatomist_ObjectMenuR( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_rc_ptr_ObjectMenu, 0 );
+  return sipConvertFromType( a, sipType_rc_ptr_ObjectMenu, 0 );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_ObjectMenu( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_anatomist_ObjectMenu, 0, 0, 0,
-                               &isErr );
+  return sipConvertToType( o, sipType_anatomist_ObjectMenu, 0, 0, 0,
+                           &isErr );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_ObjectMenuP( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_anatomist_ObjectMenu, 0, 0, 0,
-                               &isErr );
+  return sipConvertToType( o, sipType_anatomist_ObjectMenu, 0, 0, 0,
+                           &isErr );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_ObjectMenuR( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_rc_ptr_ObjectMenu, 0, 0,
-                               0, &isErr );
+  return sipConvertToType( o, sipType_rc_ptr_ObjectMenu, 0, 0,
+                           0, &isErr );
 }
 
 
 inline int pyanatomistObjectMenu_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_anatomist_ObjectMenu,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_anatomist_ObjectMenu,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 
 inline int pyanatomistObjectMenuP_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_anatomist_ObjectMenu,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_anatomist_ObjectMenu,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 
 inline int pyanatomistObjectMenuR_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_rc_ptr_ObjectMenu,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_rc_ptr_ObjectMenu,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 #endif
--- pyanatomist/src/sip/palette.h
+++ pyanatomist/src/sip/palette.h
@@ -41,66 +41,66 @@ typedef carto::rc_ptr<anatomist::APalette> rc_ptr_APalette;
 
 inline PyObject* pyanatomistConvertFrom_anatomist_APalette( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_anatomist_APalette, 0 );
+  return sipConvertFromType( a, sipType_anatomist_APalette, 0 );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_APalette( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_anatomist_APalette, 0, 0, 0,
-                               &isErr );
+  return sipConvertToType( o, sipType_anatomist_APalette, 0, 0, 0,
+                           &isErr );
 }
 
 
 inline int pyanatomistAPalette_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_anatomist_APalette,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_anatomist_APalette,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 // --
 
 inline PyObject* pyanatomistConvertFrom_anatomist_APaletteP( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_anatomist_APalette, 0 );
+  return sipConvertFromType( a, sipType_anatomist_APalette, 0 );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_APaletteP( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_anatomist_APalette, 0, 0, 0,
-                               &isErr );
+  return sipConvertToType( o, sipType_anatomist_APalette, 0, 0, 0,
+                           &isErr );
 }
 
 
 inline int pyanatomistAPaletteP_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_anatomist_APalette,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_anatomist_APalette,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 // --
 
 inline PyObject* pyanatomistConvertFrom_anatomist_APaletteR( void * a )
 {
-  return sipConvertFromInstance( a, sipClass_rc_ptr_APalette, 0 );
+  return sipConvertFromType( a, sipType_rc_ptr_APalette, 0 );
 }
 
 
 inline void* pyanatomistConvertTo_anatomist_APaletteR( PyObject * o )
 {
   int isErr = 0;
-  return sipConvertToInstance( o, sipClass_rc_ptr_APalette, 0, 0, 0,
-                               &isErr );
+  return sipConvertToType( o, sipType_rc_ptr_APalette, 0, 0, 0,
+                           &isErr );
 }
 
 
 inline int pyanatomistAPaletteR_Check( PyObject* o )
 {
-  return sipCanConvertToInstance( o, sipClass_rc_ptr_APalette,
-                                  SIP_NOT_NONE | SIP_NO_CONVERTORS );
+  return sipCanConvertToType( o, sipType_rc_ptr_APalette,
+                              SIP_NOT_NONE | SIP_NO_CONVERTORS );
 }
 
 
