9425e6cf7473b22a447ebc17fab4b0a1047b291c
Denis Rivière <denis.riviere.gm@gmail.com>
2023-10-20 20:29:58 +0200

    fixes in examples

--- pyanatomist/doc/examples/fusion.py
+++ pyanatomist/doc/examples/fusion.py
@@ -38,9 +38,6 @@ Custom fusion example
 Creating a new custom fusion type in Anatomist
 '''
 
-from __future__ import print_function
-
-from __future__ import absolute_import
 from soma import aims
 import os
 import anatomist.direct.api as anatomist
